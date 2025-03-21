9c86bfb3aef8654c872badba0696aedc9f851956
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-22 01:47:02 +0500

    ..

---
+++ some-trams-walk/src/components/layout/current_user.tsx
@@ -0,0 +1,11 @@
+import { Button, Popover } from "antd";
+
+const CurrentUser = () => {
+  return (
+    <>
+      <Popover placement="BottomRight"></Popover>
+    </>
+  );
+};
+
+export default CurrentUser;
--- some-trams-walk/src/providers/data/index.tsx
+++ some-trams-walk/src/providers/data/index.tsx
@@ -7,9 +7,9 @@ import { createClient } from "graphql-ws";
 
 import { url } from "inspector";
 
-export const API_URL = "https://api.crm.refine.dev";
-export const WS_Client = "wss://api.crm.refine.dev/graphql";
 export const API_BASE_URL = "https://api.crm.refine.dev";
+export const API_URL = `${API_BASE_URL}/graphql`; // it is the end point of api base Url of graphql
+export const WS_Client = "wss://api.crm.refine.dev/graphql";
 
 export const client = new GraphQLClient(API_URL, {
   fetch: (url: string, options: RequestInit) => {
