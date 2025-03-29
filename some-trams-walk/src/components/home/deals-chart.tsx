06d42b026a210c651feb59161b90453b81d5f9af
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-03-29 17:48:20 +0500

    Upcoming events card update

---
+++ some-trams-walk/src/components/home/deals-chart.tsx
@@ -0,0 +1,8 @@
+
+const DealsChart = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default DealsChart;
--- some-trams-walk/src/pages/Home/index.tsx
+++ some-trams-walk/src/pages/Home/index.tsx
@@ -1,5 +1,38 @@
+import { UpcomingEvents , DealsChart} from "@/components";
+import { Row , Col } from "antd";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Row
+        gutter={[32,32]}
+        style={{
+          margin: "33px"
+        }}
+      >
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px"
+          }}
+        >
+          <UpcomingEvents/>
+        </Col>
+        <Col
+          xs={24}
+          sm={24}
+          xl={8}
+          style={{
+            height: "460px"
+          }}
+        >
+          <DealsChart/>
+        </Col>
+      </Row>
+    </div>
+  )
 };
 
 export default Home;
