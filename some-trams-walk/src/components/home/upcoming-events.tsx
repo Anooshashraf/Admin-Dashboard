e42cf6bdbbb214aaa584401eebc23a8bfaa75a5f
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-04-03 16:47:57 +0500

    ..

--- some-trams-walk/src/components/home/upcoming-events.tsx
+++ some-trams-walk/src/components/home/upcoming-events.tsx
@@ -1,24 +1,34 @@
 import { CalendarOutlined } from "@ant-design/icons";
-import { Card, List } from "antd";
+import { Badge, Card, List } from "antd";
 import { Text } from "../text";
 import { useState } from "react";
 import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
+import { useList } from "@refinedev/core";
+import { getDate } from "@/utilities/helpers";
+import { DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY } from "@/graphql/queries";
 
 const UpcomingEvents = () => {
-  const [isLoading ,setIsLoading] = useState(true);
-
+  const [isLoading ,setIsLoading] = useState(false);
+  const {data, isLoading: eventsLoading}= useList({
+    resource: 'events',
+    pagination:{pageSize:5},
+    meta: {
+      gqlQuery: DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY
+    }
+  });
+  // alert(JSON.stringify(data))
   return (
     <Card 
       style={{
         height:"100%"
       }}
-      headStyle={{
+      headStyle = {{
         padding: '8px 16px'
       }}
-      bodyStyle={{
+      bodyStyle = {{
         padding: '0 1rem'
       }}
-      title={
+      title = {
         <div style={{
           display:'flex',
           alignItems: 'center',
@@ -49,7 +59,26 @@ const UpcomingEvents = () => {
 
         </List>
       ) : ( 
-        <List>
+        <List
+          itemLayout="horizontal"
+          dataSource={data?.data || []}
+          renderItem={(item)=> {
+          const renderDate = getDate(item.startDate , item.endDate)
+            return(
+              <List.Item>
+                <List.Item.Meta
+                avatar={<Badge color={item.color}/>}
+                title={<Text size="xs">{renderDate}</Text>}
+                description={<Text ellipsis={{tooltip:true}}
+                strong>
+                  {item.title}
+                </Text>}
+
+                />
+              </List.Item>
+          )
+        }}
+        >
 
         </List>
       )}
