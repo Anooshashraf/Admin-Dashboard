0d1d5984fe3f6f63e4ee3b6cc7282d494c5dd3ce
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-04-04 18:34:03 +0500

    deals-chart graph

--- some-trams-walk/src/components/home/deals-chart.tsx
+++ some-trams-walk/src/components/home/deals-chart.tsx
@@ -1,7 +1,68 @@
+import { DollarOutlined } from "@ant-design/icons";
+import { Card } from "antd";
+import { Text } from "../text";
+import { Area, AreaConfig } from "@ant-design/plots";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/queries";
+import React from "react";
+import { mapDealsData } from "@/utilities/helpers";
+import { GetFields } from "@refinedev/nestjs-query";
+
+
 
 const DealsChart = () => {
+  const {data} = useList<GetFields>(
+    {
+      resource: 'dealStages',
+      meta:{
+        gqlQuery: DASHBOARD_DEALS_CHART_QUERY
+      }
+    }
+  );
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data)
+  },[data?.data]);
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: 'timeText',
+    yField: 'value'
+  }
   return (
-    <div></div>
+    <Card
+      style={{
+        height: "100%"
+      }}
+      headStyle={{
+        padding: '8px 16px'
+      }}
+      bodyStyle={{
+        padding: '24px 24px 0 24px'
+      }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px"
+          }}
+        >
+          <DollarOutlined/>
+          <Text 
+            size="sm"
+            style={{
+              marginLeft: "0.5rem"
+            }}
+          
+          >
+            Deals
+          </Text>
+        </div>
+      }
+    
+    >
+      <Area { ...config} height={325}/>
+    </Card>
   )
 }
 
