8dd3744db1f210398df53fbd522b87fe0d0cf16e
Anooshashraf <ASHRAF4621021@cloud.neduet.edu.pk>
2025-04-11 04:15:32 +0500

    LATEST-ACTIVITIES

--- some-trams-walk/src/components/home/latest-activities.tsx
+++ some-trams-walk/src/components/home/latest-activities.tsx
@@ -1,8 +1,42 @@
 import { UnorderedListOutlined } from "@ant-design/icons";
-import { Card } from "antd";
+import { Card, List } from "antd";
 import { Text } from "../text";
+import LatestActivitiesSkeleton from "../skeleton/latest-activities";
+import { useList } from "@refinedev/core";
+import { DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY, DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY } from "@/graphql/queries";
 
 const LatestActivities = () => {
+    const {data:audit, isLoading: isLoadingAudit, isError ,error } = useList({
+        resource: 'audits',
+        meta: {
+            gqlQuery:  DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY
+            
+        }
+    })
+
+    const dealIds = audit?.data?.map((audit)=> audit?.targetId);
+    const { data: deals, isLoading: isLoadingDeals} = useList({
+        resource: 'deals',
+        queryOptions: {
+            enabled: !!dealIds?.length
+        },
+        pagination: {
+            mode: 'off'
+        },
+        filters: [
+            {
+                field:'id' ,
+                operator: 'in',
+                value: dealIds
+            }
+        ],
+        meta: {
+            gqlQuery: DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY
+        }
+
+
+    })
+    const isLoading = false;
     return(
         <Card
         headStyle={{
@@ -20,12 +54,33 @@ const LatestActivities = () => {
                     }}
                 >
                     <UnorderedListOutlined/>
-                    <Text></Text>
+                    <Text 
+                        size="sm" style={{ marginLeft: '0.5rem'}}                    
+                    >
+                        Latest Activities
+                    </Text>
                 </div>
             )
         }
         >
-             
+            {isLoading ? (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={Array.from({ length: 5}).map((_,i) => ({
+                        id:i 
+                    })
+                    )}
+                    renderItem={(_, index) => (
+                        <LatestActivitiesSkeleton key={index}/>
+                    )}
+                />
+            ):(
+                <List
+                    itemLayout="horizontal"
+                    dataSource={}
+                />
+            )
+            }
         </Card>
     )
 }
