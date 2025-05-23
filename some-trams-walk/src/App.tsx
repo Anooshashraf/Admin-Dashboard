fc832c3de0e882a0720325f1c6064b54afad8325
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-05-23 19:00:31 +0500

    ..

--- some-trams-walk/src/App.tsx
+++ some-trams-walk/src/App.tsx
@@ -69,6 +69,7 @@ function App() {
                   <Route path="/companies">
                     <Route index element={<CompanyList/>}/>
                     <Route path="new" element={<Create />}/>
+                    <Route path="edit/:id" element={<Create />}/>
                   </Route>
                 </Route>
               </Routes>
