a89f97e77ce363634fdd5685608e178b87bc5086
Anooshashraf <165590345+Anooshashraf@users.noreply.github.com>
2025-05-23 18:32:41 +0500

    names added to the company add bar

--- some-trams-walk/src/components/select-option-with-avatar.tsx
+++ some-trams-walk/src/components/select-option-with-avatar.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import CustomAvatar from './custom-avatar';
+import { Text } from './text';
 
 
 type Props = {
-    name: string,
-    avatarUrl?: string,
-    shape? : 'circle' | 'square';
+  name: string,
+  avatarUrl?: string,
+  shape? : 'circle' | 'square';
 }
 const SelectOptionWithAvatar = ({avatarUrl , name ,shape}: Props) => {
   return (
@@ -17,6 +18,7 @@ const SelectOptionWithAvatar = ({avatarUrl , name ,shape}: Props) => {
         }}
     >
         <CustomAvatar shape={shape} name={name} src={avatarUrl}/>
+        <Text>{name}</Text>
     </div>
   )
 }
